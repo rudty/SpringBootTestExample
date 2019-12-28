0c5c5153092e61c4d0a00d1ab9945de2a5b873cf
석경우 <rudtyz@gmail.com>
2019-12-28 15:47:51 +0900

    jparepository 예제 추가

--- src/test/kotlin/org/rudtyz/tests/jpa/JpaRepositoryTest.kt
+++ src/test/kotlin/org/rudtyz/tests/jpa/JpaRepositoryTest.kt
@@ -2,10 +2,14 @@ package org.rudtyz.tests.jpa
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.rudtyz.tests.Sample
 import org.rudtyz.tests.SampleRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
+import org.springframework.transaction.annotation.Isolation
 import org.springframework.transaction.annotation.Transactional
 
 @DataJpaTest
@@ -13,19 +17,84 @@ class JpaRepositoryTest {
     @Autowired
     lateinit var repository: SampleRepository
 
-    @Test
-    @Transactional
-    fun save_and_load_all() {
+    private fun insertDummyData1000() {
+        for (i in 1..1000) {
+            val s = Sample(
+                    name = "rep_sample $i",
+                    number = i
+            )
+            repository.save(s)
+        }
+    }
+
+    private fun insertDummyData1() {
         val s = Sample(
                 name = "rep_sample",
                 number = 3
         )
         repository.save(s)
+    }
 
+    @Test
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    fun save_and_load_all() {
+        insertDummyData1()
         val l = repository.findAll()
         assertThat(l).isNotNull
         assertThat(l).isNotEmpty
     }
 
 
+    @Test
+    fun select_one_1_not_found_throw() {
+//        insertDummyData1()
+        assertThrows<RuntimeException> {
+            repository.getOne("rep_sample")
+        }
+    }
+
+    @Test
+    fun select_one_1_not_found_optional() {
+        val elem = repository.findById("rep_sample")
+        assertThat(elem.isEmpty).isTrue()
+    }
+
+    @Test
+    fun select_one_1_found_optional() {
+        insertDummyData1()
+        val elem = repository.findById("rep_sample")
+        assertThat(elem.isEmpty).isFalse()
+    }
+
+    @Test
+    fun select_offset_limit() {
+        insertDummyData1000()
+
+        /**
+         * select *
+         * from sample
+         * order by
+         *   number desc
+         * limit 10 offset 20
+         *
+         * limit = size
+         * offset = page * size
+         */
+        val pageRequest = PageRequest.of(
+                /*page*/2,
+                /*size*/10,
+                Sort.Direction.DESC,
+                /*number 로 order by */"number")
+        val queryResult = repository.findAll(pageRequest)
+        assertThat(queryResult).isNotNull
+        assertThat(queryResult).isNotEmpty
+
+        val list = queryResult.content
+        assertThat(list).isNotNull
+
+        val firstElem = list[0]
+        assertThat(firstElem).isNotNull
+        assertThat(firstElem.name).isEqualTo("rep_sample 980")
+        assertThat(firstElem.number).isEqualTo(980)
+    }
 }
