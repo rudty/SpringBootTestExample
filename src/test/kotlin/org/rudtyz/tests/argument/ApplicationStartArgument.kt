306bdfa23413b60f4be486a266af72823485fdbc
석경우 <rudtyz@gmail.com>
2020-01-01 00:44:16 +0900

    시작 인자 테스트

---
+++ src/test/kotlin/org/rudtyz/tests/argument/ApplicationStartArgument.kt
@@ -0,0 +1,24 @@
+package org.rudtyz.tests.argument
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(args = ["--app.t1=123", "--app.t2=456"])
+class ApplicationStartArgument {
+    @Autowired
+    lateinit var args: ApplicationArguments
+
+    @Test
+    fun argv() {
+        assertThat(args.optionNames).contains("app.t1")
+        assertThat(args.optionNames).contains("app.t2")
+
+        assertThat(args.getOptionValues("app.t1")).contains("123")
+        assertThat(args.getOptionValues("app.t2")).contains("456")
+        assertThat(args.getOptionValues("app.t3")).isNull()
+    }
+
+}
