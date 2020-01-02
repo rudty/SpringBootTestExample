670c1d6ca83f588116a329faabedc6966a653d31
석경우 <rudtyz@gmail.com>
2020-01-02 19:45:34 +0900

    TestConfig 추가

---
+++ src/test/kotlin/org/rudtyz/tests/bean/TestConfig.kt
@@ -0,0 +1,38 @@
+package org.rudtyz.tests.bean
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Import
+
+@TestConfiguration
+class HelloWordConfig1 {
+    @Bean
+    fun helloWorldBean() = "HelloWorld1"
+}
+
+@TestConfiguration
+class HelloWordConfig2 {
+    @Bean
+    fun helloWorldBean() = "HelloWorld2"
+}
+
+@SpringBootTest
+@Import(HelloWordConfig1::class)
+class TestConfig {
+    /**
+     * Qualifier 는 실행에 영향을 주지않습니다만
+     * IDE 의 경고를 제거합니다
+     */
+    @Qualifier("helloWorldBean")
+    @Autowired
+    lateinit var helloWorldBean: String
+
+    @Test
+    fun helloWorld() {
+        print(helloWorldBean)
+    }
+}
